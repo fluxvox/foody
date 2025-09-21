a866124964a998515660b19fc0de6bb0e070d0e6
fluxvox <60875822+fluxvox@users.noreply.github.com>
2025-09-21 11:29:45 +0200

    fixed some configs

--- config.py
+++ config.py
@@ -14,10 +14,10 @@ class Config:
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'false').lower() in ('true', '1', 'yes')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = [os.environ.get('ADMINS') or 'admin@lab10.ifalabs.org']
+    ADMINS = [os.environ.get('ADMINS') or 'student@lab10.ifalabs.org']
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     # Disable Elasticsearch and Redis for local deployment
